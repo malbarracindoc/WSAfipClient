e3bc2b0a7158c9ff96b69074d6d165cb7b7dd5c2
marcelo <marceloalejandro.albarracin@gmail.com>
2018-10-25 23:29:48 -0300

    Primer Entrega

---
+++ src/com/liciusit/ws/afip/client/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ar.gov.afip.dif.FEV1/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.liciusit.ws.afip.client.types;
